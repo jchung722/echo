4474441e646155bc0935973ba921c25ff65e246b
Jessica Chung <jchung722@gmail.com>
2019-06-27 11:59:52 -0500

    first commit

---
+++ src/main/java/com/eighthlight/EchoServer.java
@@ -0,0 +1,29 @@
+package com.eighthlight;
+
+import java.net.*;
+import java.io.*;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        try (
+                ServerSocket serverSocket = new ServerSocket(3333);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader( new InputStreamReader((clientSocket.getInputStream())));
+        ) {
+            out.println("Enter goodbye to exit.");
+            boolean done = false;
+            String inputLine;
+            while (!done && (inputLine = in.readLine()) != null) {
+                out.println("Server says:" + inputLine);
+
+                if(inputLine.toLowerCase().trim().equals("goodbye")){
+                    done = true;
+                }
+            }
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/com/eighthlight/EchoServerTest.java
@@ -0,0 +1,11 @@
+package com.eighthlight;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class EchoServerTest {
+    @Test
+    public void firstTest() {
+        Assert.assertEquals(1, 1);
+    }
+}
